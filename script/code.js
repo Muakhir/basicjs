5b6f460dcfd6289cb3750c9f9b6cb3870295bc22
Muakhir <almuakhirjones@gmail.com>
2023-10-10 10:43:42 +0200

    folder structure

--- script/code.js
+++ script/code.js
@@ -1,6 +1,6 @@
 let firstname = 'Almuakhir'
 console.log("Hello my name is"+ firstname)
-console.log(firstname + "has 7 sisters")
+console.log(firstname + " has 7 sisters")
 
 console.log(`My name is ${firstname}`)
-console.log(`${firstname}has 7 sisters`)
+console.log(`${firstname} has 7 sisters`)

052a090695d16747ef7bcd857239018dcde4eb85
Muakhir <almuakhirjones@gmail.com>
2023-10-10 10:30:10 +0200

    folder structure

--- script/code.js
+++ script/code.js
@@ -1,2 +1,6 @@
-console.log("Hello my name is Almuakhir Jones")
-console.log("Joel has 7 sisters")
+let firstname = 'Almuakhir'
+console.log("Hello my name is"+ firstname)
+console.log(firstname + "has 7 sisters")
+
+console.log(`My name is ${firstname}`)
+console.log(`${firstname}has 7 sisters`)
